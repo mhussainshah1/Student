eaabfbd0c1389253d3714d6172354938b08ca40e
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2021-07-16 00:49:40 -0400

    Student

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -7,8 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface StudentRepository
-        extends JpaRepository<Student,Long> {
+public interface StudentRepository extends JpaRepository<Student,Long> {
 
     @Query("SELECT s from Student s WHERE s.email =?1") //Student is class not table in database
     Optional<Student> findStudentByEmail(String email);
